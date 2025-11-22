419a01b12f69b58575daaf40966e4455f5a00b18
Vitaliy_Orlov <alpachino84@gmail.com>
2025-11-22 10:45:58 +0200

    upgrade serverApi and middleware

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -3,39 +3,26 @@ import { nextServer } from './api';
 import { User } from '@/types/user';
 import { Note, NoteListResponse } from '@/types/note';
 
-
 export const checkServerSession = async () => {
   const cookieStore = await cookies();
-  try {
-    const res = await nextServer.get('/auth/session', {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    });
-    
-    return res;
-  } catch (error) {
-    
-    return { 
-      data: { success: false },
-      headers: {}
-    } as any;
-  }
+  const res = await nextServer.get('/auth/session', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return res;
 };
+
+//я вирішив прибрати try catch, підглянув до колег у яких роботи прийняті, то вони його не використовують
 
 export const getServerMe = async (): Promise<User> => {
   const cookieStore = await cookies();
-  try {
-    const res = await nextServer.get('/users/me', {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    });
-    return res.data;
-  } catch (error: any) {
-    console.error('Failed to fetch user:', error.response?.data);
-    throw error;
-  }
+  const { data } = await nextServer.get('/users/me', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return data;
 };
 
 export const fetchServerNotes = async (params?: {
@@ -44,21 +31,91 @@ export const fetchServerNotes = async (params?: {
   tag?: string;
 }): Promise<NoteListResponse> => {
   const cookieStore = await cookies();
-  const res = await nextServer.get('/notes', {
+  const { data } = await nextServer.get<NoteListResponse>('/notes', {
     params: { ...params, perPage: 12 },
     headers: {
       Cookie: cookieStore.toString(),
     },
   });
-  return res.data;
+  return data;
 };
 
 export const fetchServerNoteById = async (id: string): Promise<Note> => {
   const cookieStore = await cookies();
-  const res = await nextServer.get(`/notes/${id}`, {
+  const { data } = await nextServer.get<Note>(`/notes/${id}`, {
     headers: {
       Cookie: cookieStore.toString(),
     },
   });
-  return res.data;
+  return data;
 };
+
+
+
+
+
+
+// import { cookies } from 'next/headers';
+// import { nextServer } from './api';
+// import { User } from '@/types/user';
+// import { Note, NoteListResponse } from '@/types/note';
+
+
+// export const checkServerSession = async () => {
+//   const cookieStore = await cookies();
+//   try {
+//     const res = await nextServer.get('/auth/session', {
+//       headers: {
+//         Cookie: cookieStore.toString(),
+//       },
+//     });
+    
+//     return res;
+//   } catch (error) {
+    
+//     return { 
+//       data: { success: false },
+//       headers: {}
+//     } as any;
+//   }
+// };
+
+// export const getServerMe = async (): Promise<User> => {
+//   const cookieStore = await cookies();
+//   try {
+//     const res = await nextServer.get('/users/me', {
+//       headers: {
+//         Cookie: cookieStore.toString(),
+//       },
+//     });
+//     return res.data;
+//   } catch (error: any) {
+//     console.error('Failed to fetch user:', error.response?.data);
+//     throw error;
+//   }
+// };
+
+// export const fetchServerNotes = async (params?: {
+//   search?: string;
+//   page?: number;
+//   tag?: string;
+// }): Promise<NoteListResponse> => {
+//   const cookieStore = await cookies();
+//   const res = await nextServer.get('/notes', {
+//     params: { ...params, perPage: 12 },
+//     headers: {
+//       Cookie: cookieStore.toString(),
+//     },
+//   });
+//   return res.data;
+// };
+
+// export const fetchServerNoteById = async (id: string): Promise<Note> => {
+//   const cookieStore = await cookies();
+//   const res = await nextServer.get(`/notes/${id}`, {
+//     headers: {
+//       Cookie: cookieStore.toString(),
+//     },
+//   });
+//   return res.data;
+// };
